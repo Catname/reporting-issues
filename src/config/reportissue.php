7ec97239c9a8b7ff74f0877a8cdf440cda85a1bb
ZhangHQ <tomcath@foxmail.com>
2021-11-01 17:14:30 +0800

    需要用到的配置信息

---
+++ src/config/reportissue.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author: ZhangHQ
+ * @email : tomcath@foxmail.com
+ */
+
+return [
+    'report_host' => env('REPORT_HOST', 'http://issue.test/'),
+    'client_id' => env('REPORT_ID'),
+    'client_secret' => env('REPORT_SECRET'),
+    'sign_halt' => env('REPORT_SECRET'),
+];
