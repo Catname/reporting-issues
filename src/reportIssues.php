b073cf87c5901712a027b03e3007b5fc9dade51d
ZhangHQ <tomcath@foxmail.com>
2021-11-01 17:14:08 +0800

    上报工单 相关方法

---
+++ src/reportIssues.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author: ZhangHQ
+ * @email : tomcath@foxmail.com
+ */
+
+namespace Catname\ReportingIssues;
+
+
+
+class reportIssues
+{
+    /**
+     * 上报工单
+     * @param string $message
+     * @param array $data
+     * @return string
+     * @author: ZhangHQ
+     * @email : tomcath@foxmail.com
+     */
+    public static function report(string $message, array $data)
+    {
+        $content = $message . ' ；相关错误信息：[ ' . json_encode($data) . ']';
+        $url = config('reportissue.report_host') . 'api/admin/reports';
+        $accessToken = authorizations::getAccessToken();
+
+        $signHandler = new SignHandler();
+        $timeStamp = now()->getTimestamp();
+
+        $contentData = [
+            'issue_content' => $content,
+            //'media' => $data['media'], // Json
+        ];
+
+        try {
+            $response = \Http::withToken($accessToken)->withHeaders([
+                'sign' => $signHandler->getASign('POST', $timeStamp, $contentData),
+                'timestamp' => $timeStamp,
+            ])->post($url, $contentData);
+        } catch (\Exception $e) {
+            // TODO 错误处理
+            return $e->getMessage();
+        }
+
+        return $response->body();
+    }
+}
